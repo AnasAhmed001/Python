7fc74d174e0af9ff146f0363c90336249f9dc80a
AnasAhmed001 <hafizanasahmed8@gmail.com>
2025-04-13 23:08:34 +0500

    control flow

--- Control-Flow/main.py
+++ Control-Flow/main.py
@@ -86,3 +86,98 @@ print(fs)
 
 # fs.add(4) → ❌ Error: frozenset is immutable
 
+# control modules and functions
+# A module is a file containing Python code. A function is a block of reusable code.
+# You can create your own modules and functions.
+
+# function definition and call
+
+def greet(name):
+    print("Hello,", name)
+
+greet("Alice")
+
+# importing modules
+
+import math
+
+print(math.sqrt(16))  # 4.0
+
+# importing specific functions
+
+from math import pi
+
+print(pi)  # 3.141592653589793
+
+# Exceptin handling
+# Exceptions are errors that occur during the execution of a program.
+# You can handle exceptions using try and except blocks.
+
+# try-except block
+
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+except ValueError:
+    print("Invalid input. Please enter a number.")
+
+
+# try-except-finally block
+
+try:
+    x = int("hello")
+except ValueError:
+    print("Conversion failed.")
+finally:
+    print("This block always runs.")
+
+
+# file handling
+# Python provides built-in functions to handle files.
+# You can read from and write to files.
+
+# writing to a file
+with open("example.txt", "w") as file:
+    file.write("Hello, World!")
+
+# reading from a file
+with open("sample.txt", "r") as file:
+    content = file.read()
+    print(content)
+    
+
+# appending to a file
+
+with open("example.txt", "a") as file:
+    file.write("\nAppending a new line.")
+
+# using math module
+
+import math
+
+print(math.pow(2, 3))  # 8.0
+print(math.factorial(5))  # 120
+
+# using date time module
+
+from datetime import datetime
+
+now = datetime.now()
+print("Current date and time:", now)
+print("Year:", now.year)
+print("Month:", now.month)
+print("Day:", now.day)
+print("Hour:", now.hour)
+
+# using calendar module
+
+import calendar
+
+# Print calendar for April 2025
+print(calendar.month(2025, 4))
+
+# Check if a year is leap year
+print(calendar.isleap(2024))  # True
